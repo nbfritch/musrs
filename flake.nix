a70420b7c2de198d8709105feff4c83e7b320a7e
Nathan Fritch <nbfritch@gmail.com>
2024-04-03 20:11:39 -0500

    Precache sqlx queries, add nix build with naersk

--- flake.nix
+++ flake.nix
@@ -1,38 +1,58 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
+  outputs = { self, flake-utils, naersk, nixpkgs, nixpkgs-mozilla }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [
+            (import nixpkgs-mozilla)
+          ];
+        };
+        toolchain = (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchain.toml;
+          sha256 = "sha256-xaVCskjfBe+XQHSbPi9OwGzLCc9BaWvi5wA/BZQGu0Q=";
+          #        ^ After you run `nix build`, replace this with the actual
+          #          hash from the error message
+        }).rust;
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in {
+        packages = {
+          default = naersk'.buildPackage {
+            src = ./.;
           };
-          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            buildInputs = [
+          check = naersk'.buildPackage {
+            src = ./.;
+            mode = "check";
+          };
+          clippy = naersk'.buildPackage {
+            src = ./.;
+            mode = "clippy";
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+              toolchain
+              sqlx-cli          
               bashInteractive
-              rustToolchain
               sqlite
               rust-analyzer
               bun
               nodePackages.typescript-language-server
-            ];
-          };
-        }
-      );
+          ];
+        };
+      }
+    );
 }
-
